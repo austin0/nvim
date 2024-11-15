c79cd4aa0b1f929ffbdc53f0b22c88630ef082f3
Austin Bailey <Austin.Bailey@ibm.com>
2024-11-15 02:51:21 +0000

    fix: avante build for macos

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -8,11 +8,9 @@ return {
   lazy = false,
   version = false, -- set this if you want to always pull the latest change
   opts = {
-    -- add any opts here
+    provider = 'openai',
   },
-  -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
-  build = 'make',
-  -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  build = 'make BUILD_FROM_SOURCE=true',
   dependencies = {
     'nvim-treesitter/nvim-treesitter',
     'stevearc/dressing.nvim',
