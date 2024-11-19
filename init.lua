3b52c304e1bf29cf6dfd966581bdf2258d2758b1
austin0 <austin@0xaustin.co.uk>
2024-11-19 04:32:12 +0000

    fix: re-enable nu lsp features

--- init.lua
+++ init.lua
@@ -493,7 +493,6 @@ require('lazy').setup({
       { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
       'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
-      'nvimtools/none-ls.nvim',
       -- Useful status updates for LSP.
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { 'j-hui/fidget.nvim', opts = {} },
